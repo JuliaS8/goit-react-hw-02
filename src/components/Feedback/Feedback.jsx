29473da3f47b85cb9751b56c142f344568099a20
yuliia_sharapova <juliaa.sharapovaa@gmail.com>
2024-07-28 22:41:42 +0300

    updates 2 from 28.07

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,14 +4,13 @@ import Notification from '../Notification/Notification';
 const Feedback = ({ good, neutral, bad, totalFeedback, positiveFeedback }) => {
   return (
     <div className={css.feedback}>
-      {totalFeedback === 0 ? (<Notification />) : (
         <ul className={css.feedbackList}>
           <li>Good: {good}</li>
           <li>Neutral: {neutral}</li>
           <li>Bad: {bad}</li>
           <li>Total: {totalFeedback}</li>
           <li>Positive: {positiveFeedback}%</li>
-        </ul>)}
+        </ul>
       </div>
 )
 }
